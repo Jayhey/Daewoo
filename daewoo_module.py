00b5d7569322478c9c97c93cb99332cb59cc0e13
Jayhey <winga1@korea.ac.kr>
2018-05-22 20:39:23 +0900

    classification 추가

--- daewoo_module.py
+++ daewoo_module.py
@@ -3,6 +3,8 @@ import numpy as np
 
 NUM_CLASSES = 3
 
+
+# input 설정. 경로 string을 텐서로 바꿈
 def set_input(img, label):
 	np.random.seed(1234)
 	idx = np.random.permutation(len(img))
@@ -22,6 +24,7 @@ def set_input(img, label):
 	return train_img_tensor, train_label_tensor, test_img_tensor, test_label_tensor
 
 
+# string 텐서를 img 텐서로 변환 후 crop
 def input_tensor_regression(img_path, label):
 	img_file = tf.read_file(img_path)
 	img_decoded = tf.image.decode_png(img_file)
@@ -30,12 +33,14 @@ def input_tensor_regression(img_path, label):
 	label = tf.cast(label, tf.float32)
 	return img_crop, label
 
+# string 텐서를 img 텐서로 변환 후 crop (classification)
 def input_tensor(img_path, label):
 	img_file = tf.read_file(img_path)
 	img_decoded = tf.image.decode_png(img_file)
 	img_float = tf.to_float(img_decoded)
 	img_crop = tf.random_crop(img_float, size=[270, 270, 3])
 	label = tf.one_hot(label, NUM_CLASSES)
+
 	return img_crop, label
 
 def conv2d(x, num_outputs, batch_norm=True):
@@ -57,11 +62,18 @@ def pooling(x):
 	return pool
 
 
+def dense(x, output, fn=tf.nn.relu, batch_norm=True):
+	if batch_norm is True:
+		fc_bn = tf.contrib.layers.batch_norm
+	else:
+		fc_bn = None
+	fc = tf.contrib.layers.fully_connected(inputs=x,
+	                                       num_outputs=output,
+	                                       normalizer_fn=fc_bn,
+	                                       activation_fn=fn)
+	return fc
 
 class VGG16():
-	'''
-	VGG 16Network
-	'''
 	def __init__(self, x, y, bn, classification):
 
 		with tf.name_scope("layer_1"):
@@ -94,22 +106,24 @@ class VGG16():
 
 		with tf.name_scope("FC_layer"):
 			fc1 = tf.layers.flatten(pool5)
-			fc2 = tf.layers.dense(fc1, 4096, activation=tf.nn.relu)
-			fc3 = tf.layers.dense(fc2, 4096, activation=tf.nn.relu)
+			fc2 = dense(fc1, 4096, batch_norm=bn)
+			fc3 = dense(fc2, 4096, batch_norm=bn)
 
 		self.learning_rate = tf.placeholder(tf.float32)
+		self.global_step = tf.Variable(0, trainable=False, name='global_step')
 
 		if classification is True:
-			self.logits = tf.layers.dense(fc3, NUM_CLASSES, activation=tf.nn.relu)
-			self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=y))
+			self.logits = dense(fc3, NUM_CLASSES, fn=None, batch_norm=True)
+			self.loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=self.logits)
+			# self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
 			self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate)
-			self.global_step = tf.Variable(0, trainable=False, name='global_step')
+
 			self.train = self.optimizer.minimize(self.loss)
 
 			self.y_prob = tf.nn.softmax(self.logits)
-			self.y_pred = tf.argmax(self.logits, 1)
+			self.y_pred = tf.argmax(self.y_prob, 1)
 
-			self.correct_prediction = tf.equal(self.y_pred, tf.cast(y, tf.int64))
+			self.correct_prediction = tf.equal(self.y_pred, tf.arg_max(y, 1))
 			self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
 
 			tf.summary.scalar("accuray", self.accuracy)
@@ -118,9 +132,7 @@ class VGG16():
 		else:
 			self.logits = tf.layers.dense(fc3, 1, activation=tf.nn.relu)
 			self.loss = tf.losses.mean_squared_error(labels=y, predictions=self.logits)
-			self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate)
-
-			self.global_step = tf.Variable(0, trainable=False, name='global_step')
+			self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
 			self.train = self.optimizer.minimize(self.loss)
 
 			tf.summary.scalar("loss", self.loss)
